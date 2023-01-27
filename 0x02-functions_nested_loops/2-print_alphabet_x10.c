b5c55d0a9230784f4fdb317d37dd12d108180da6
Piperkfb <piperkfb@gmail.com>
2023-01-27 11:10:03 -0800

    try me checker

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - As says
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int x;
+
+	while (x <= 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		((x++));
+	}
+}
