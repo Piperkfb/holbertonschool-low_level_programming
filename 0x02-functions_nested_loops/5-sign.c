e8c4b75ba22b49f437e98a8c7e4f066936fe5cb9
Piperkfb <piperkfb@gmail.com>
2023-01-29 08:29:31 -0800

    putchar this tiome

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_sign - prints +,-, or 0 relating to variable n
@@ -12,14 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		_putchar('+');
 		return (1);
 	}
-	if (n < 0)
+	else if (n < 0)
 	{
-		printf("-");
+		_putchar('-');
 		return (-1);
 	}
-	printf("0");
-	return (0);
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
