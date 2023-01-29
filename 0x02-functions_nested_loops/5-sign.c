5288a0490f42984768b323668895225f0aae143a
Piperkfb <piperkfb@gmail.com>
2023-01-29 08:16:56 -0800

    i think i got it

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - prints +,-, or 0 relating to variable n
+ * @n: the number that is checked
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+}
