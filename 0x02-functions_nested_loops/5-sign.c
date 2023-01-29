2fc35243be19b40139a2e5f2a2ef55ba04508bd6
Piperkfb <piperkfb@gmail.com>
2023-01-29 08:22:18 -0800

    try this on for size

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,11 +11,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		printf("+");
 		return (1);
 	}
 	if (n < 0)
 	{
+		printf("-");
 		return (-1);
 	}
-	return (0);
+	if (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	return (void)
 }
