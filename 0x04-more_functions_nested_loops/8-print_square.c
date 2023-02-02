af81ed59633b0ab46d3e0ed4454af352e5edd435
Piperkfb <piperkfb@gmail.com>
2023-02-02 11:34:27 -0800

    try this one for size 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints # as a square. determined by size number
+ * @size: The height and width of the square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	x = 0;
+
+	while (x < size)
+	{
+		for (y = 0; y < size; y++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+		(x++);
+	}
+}
