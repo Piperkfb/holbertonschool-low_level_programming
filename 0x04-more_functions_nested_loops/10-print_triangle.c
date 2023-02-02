86994e0892fa18549a0b80eba7931157a03b5716
Piperkfb <piperkfb@gmail.com>
2023-02-02 12:11:25 -0800

    kk try me

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle based on size
+ * @size: The number that determines the size
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int m;
+	int z;
+	int y;
+	int x;
+	int space;
+
+	m = 0;
+	z = 0;
+
+	if (size > 0)
+	{
+		space = (size - 1);
+
+		while (m < size)
+		{
+			for (x = space; x > 0; x--)
+			{
+				_putchar(' ');
+			}
+			(z++);
+			for (y = 0; y < z; y++)
+			{
+				_putchar(35);
+			}
+			(m++);
+			(space--);
+		}
+	}
+}
