394c9f6d3a46aefb5d4b12aeecec2c4121406821
Piperkfb <piperkfb@gmail.com>
2023-02-03 09:37:28 -0800

    betty correct

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * fizzbuzzer - Writes 1 - 100, replacing dividens of 3 with 'Fizz' and dividens 5 with 'Buzz'. If both, 'Fizz Buzz'
+ * main - Writes 1 - 100, Replace % 3 with Fizz and 5 with Buzz.
  *
  * Return: void
  */
@@ -10,21 +10,22 @@
 int main(void)
 {
 	int x;
+
 	x = 1;
 
 	while (x <= 100)
 	{
-		if ((x % 3 == 0)&&(x % 5 == 0))
+		if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			printf("FizzBuzz");
 			(x++);
 		}
-		else if ((x % 3 == 0)&&(x % 5 != 0))
+		else if ((x % 3 == 0) && (x % 5 != 0))
 		{
 			printf("Fizz");
 			(x++);
 		}
-		else if ((x % 5 == 0)&&(x % 3 != 0))
+		else if ((x % 5 == 0) && (x % 3 != 0))
 		{
 			printf("Buzz");
 			(x++);
