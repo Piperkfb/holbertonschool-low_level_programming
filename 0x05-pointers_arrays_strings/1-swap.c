7adb6245210f54ec8ee9670cb1cbefcdf96f99b3
Piperkfb <piperkfb@gmail.com>
2023-02-06 10:35:37 -0800

    try that

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Takes two pointers and swaps values
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
