91bf492286557935ecfb7281603c2e9d8a435900
Piperkfb <piperkfb@gmail.com>
2023-02-06 11:10:07 -0800

    works?

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints string followed by a new line
+ * @str: The string that is placed
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	_putchar(*str);
+}
