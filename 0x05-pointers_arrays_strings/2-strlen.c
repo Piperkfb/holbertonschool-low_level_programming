ccacdc49befb03102f1b013d343c45950cee58d9
Piperkfb <piperkfb@gmail.com>
2023-02-06 10:45:01 -0800

    Maybe?

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,16 @@
 #include "main.h"
 
 /**
+ * _strlen - Returns length of a string
+ * @s The string
  *
- *
- *
- *
+ * Return: Length of string
+ */
+
+int _strlen(char *s)
+{
+	int sl;
+
+	sl = strlen(*s)
+	return(sl);
+}
