7b89d599d2f7113a695de9577f28bea31996d830
Piperkfb <piperkfb@gmail.com>
2023-02-08 10:02:48 -0800

    try it please

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * _strcat - appends src string to dest string
+ * @dest: 
+ * @src:
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char ptr;
+	int s3l = 0;
+	int sc = 0;
+
+	/* Destination added to Return */
+	while (*dest[sc] != '\0')
+	{
+		ptr[s3l] = *dest[sc];
+		s3l++;
+		sc++;
+	}
+	sc = 0;
+	
+	/* Source added to Return */
+	while (*src[sc] != '\0')
+	{
+		ptr[s3l] = *src[sc];
+		s3l++;
+		sc++;
+	}
+	ptr[s3l] = '\0';
+	/* Destination overwritten to Return */
+	*dest = ptr;
+
+	return (ptr);
+}
