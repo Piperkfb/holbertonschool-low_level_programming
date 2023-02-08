b0c26791d1774ab1499e3170be7da7754d79b3ca
Piperkfb <piperkfb@gmail.com>
2023-02-08 13:15:38 -0800

    just a regular ol push

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares s1 and s2
+ * @s1: number 1
+ * @s2: number 2
+ *
+ * Return: 0 if equal -15 is less than 15 if greater than
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (s1 != '\0' && s2 != '\0')
+	{
+		if (s1[1..10] == s2[1..10])
+		{
+			return (0);
+		}
+	}
+}
