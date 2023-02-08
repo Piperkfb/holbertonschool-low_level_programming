ba25b29c18dfe085d1a7b8fa3e391e95d793bc11
Piperkfb <piperkfb@gmail.com>
2023-02-08 13:39:26 -0800

    ghmmmm

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,21 +10,32 @@
 int _strcmp(char *s1, char *s2)
 {
 	int n = 0;
+	char a;
+	char b;
 
 	while (s1 != '\0' && s2 != '\0')
 	{
-		if (s1[n] == s2[n])
+		if (s1[n] = s2[n])
 		{
 			n++;
 		}
-		if (s1[n] < s2[n])
+		else
 		{
-			return (-15);
-		}
-		if (s1[n] > s2[n])
-		{
-			return (15);
+		a = s1[n];
+		b = s2[n];
+		
+			if (a < b)
+			{
+				return (-15);
+			}
+			if (a > b)
+			{
+				return (15);
+			}
 		}
 	}
-	return (0);
+	if (s1[n] = '\0' && s2 = '\0')
+	{
+		return (0);
+	}
 }
