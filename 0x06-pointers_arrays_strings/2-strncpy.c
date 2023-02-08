63c565a7a8e1f8e73d37d778ca2f249053cdee6d
Piperkfb <piperkfb@gmail.com>
2023-02-08 12:40:33 -0800

    first try?

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - Copies a string
+ * @dest: Destination
+ * @src: Source
+ * @n: Number
+ *
+ * Return: 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int sc = 0;
+	int sc2 = 0;
+
+	/* n number of source relacing destination */
+	while (src[sc2] <= src[n])
+	{
+		dest[sc] = src[sc2];
+		sc++;
+		sc2++;
+	}
+	return (dest);
+}
