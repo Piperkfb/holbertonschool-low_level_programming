5dc283f46e9ee26be1be5bdd29bb3a889afc1b78
Piperkfb <piperkfb@gmail.com>
2023-02-08 10:24:15 -0800

    hmmm

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,28 +10,28 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char ptr[98];
+	char r[98];
 	int s3l = 0;
 	int sc = 0;
 
 	/* Destination added to Return */
-	while (*dest[sc] != '\0')
+	while (dest[sc] != '\0')
 	{
-		ptr[s3l] = *dest[sc];
+		r[s3l] = dest[sc];
 		s3l++;
 		sc++;
 	}
 	sc = 0;
 
 	/* Source added to Return */
-	while (*src[sc] != '\0')
+	while (src[sc] != '\0')
 	{
-		ptr[s3l] = *src[sc];
+		r[s3l] = src[sc];
 		s3l++;
 		sc++;
 	}
-	ptr[s3l] = '\0';
+	r[s3l] = '\0';
 	/* Destination overwritten to Return */
-
+	*ptr = r;
 	return (*ptr);
 }
