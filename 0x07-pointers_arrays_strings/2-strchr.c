36d99482ce0f2f449781ea36b56fb0042271b777
Piperkfb <piperkfb@gmail.com>
2023-02-11 09:10:58 -0800

    let's see first try

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: the string
+ * @c: the character
+ *
+ * Return: pointer to character if found, null if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int sc = 0;
+	char *p;
+
+	while (s[sc] != '\0')
+	{
+		if (s[sc] == c)
+		{
+			p = s[sc];
+			return (p);
+		}
+		else
+		{
+			sc++;
+		}
+	}
+	return (NULL);
+}
