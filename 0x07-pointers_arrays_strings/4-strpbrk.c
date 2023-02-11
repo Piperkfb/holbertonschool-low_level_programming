0dd69a4e6b39da1cb8c107afe676e4b9daddc147
Piperkfb <piperkfb@gmail.com>
2023-02-11 10:11:13 -0800

    a fail huh?

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strbrk - Seaches a string for any set of bytes
+ * _strpbrk - Seaches a string for any set of bytes
  * @s: the string
  * @accept: The set of bytes
  *
@@ -13,7 +13,7 @@ char *_strpbrk(char *s, char *accept)
 	int sc = 0;
 	int ac;
 	char *p;
-	
+
 	while (s[sc] != '\0')
 	{
 		for (ac = 0; accept[ac] != '\0'; ac++)
