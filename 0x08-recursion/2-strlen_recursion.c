dd5109b95609386dd7ccbcd3f965a78eb2652543
Piperkfb <piperkfb@gmail.com>
2023-02-16 10:09:40 -0800

    really?

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,11 +12,8 @@ int _strlen_recursion(char *s)
 
 	if (*s)
 	{
-		len = _strlen_recursion(s + 1);
+		len++;
+		len += _strlen_recursion(s + 1);
 	}
-	else
-	{
 		return (len);
-	}
-	return(-1);
 }
