a30acdaf7993cc043b457f423cfd4338d7f7baff
Piperkfb <piperkfb@gmail.com>
2023-02-19 10:23:02 -0800

    2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,19 +9,19 @@
 
 int main(int argc, char *argv[])
 {
-		int x, i, j;
+	int x, i, j;
 
-		if (argc != 3)
-		{
-				printf("Error\n");
-					return (1);
-		}
-		else
-		{
-				i = atoi(argv[1]);
-					j = atoi(argv[2]);
-						x = i * j;
-							printf("%d\n", x);
-		}
-		return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		x = i * j;
+		printf("%d\n", x);
+	}
+	return (0);
 }
