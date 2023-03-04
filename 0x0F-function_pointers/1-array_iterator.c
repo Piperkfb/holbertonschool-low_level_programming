1099dae13a984cdde872d70e9c05492e2659a4d7
Piperkfb <piperkfb@gmail.com>
2023-03-04 13:51:58 -0800

    curious

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,29 @@
+#include "function_pointer.h"
+
+/**
+ * array_iterator - Takes elements of an array and plugs them into the action function
+ * @array: The array
+ * @size: The number of elements
+ * @action: The function to plug into
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int sc = 0;
+	int x;
+	int count = 0;
+
+	if (array == NULL || size <= 0 || action == NULL)
+		return;
+	while (count <= size)
+	{
+
+		for (sc = 0; array[sc]; sc++)
+		{
+			x = array[sc];
+			action(x);
+			count++;
+		}
+	}
+}
