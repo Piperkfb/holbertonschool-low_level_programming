134bdd61b7b260d1293e6294263f032b1d6dda70
Piperkfb <piperkfb@gmail.com>
2023-03-04 10:08:22 -0800

    brub

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,10 @@
 #include "dog.h"
 #include <stdlib.h>
 
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+char *_strcpy(char *s2, char *s1);
+
 /**
  * new_dog - Creates a new dog_t
  * @name: Name of new dog
@@ -8,21 +12,6 @@
  * @owner: Owner of new dog
  * Return: Memory of new dog
  */
-/**
- * _strcpy - Copies a string into another area
- * @s1: The copy name
- * @s2: location of copy
- * Return: Pointer to copied string
- */
-/**
- * _strlen - captures the length of a string
- * @s: the string
- * Return: The length
- */
-
-int _strlen(char *s);
-char *_strcpy(char *s1, char *s2);
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *Watson;
@@ -30,24 +19,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	Watson = malloc(sizeof(dog_t));
 	if (Watson == NULL)
 	{
-		free(Watson);
 		return (NULL);
 	}
+	if (name == NULL)
+		Watson->name = NULL;
 
 	Watson->name = malloc(_strlen(name) + 1);
 	if (Watson->name == NULL)
 	{
-		free(Watson->name);
+		free(Watson);
 		return (NULL);
 	}
 	Watson->name = _strcpy(Watson->name, name);
 
 	Watson->age = age;
+	if (owner == NULL)
+		Watson->owner = NULL;
 
 	Watson->owner = malloc(_strlen(owner) + 1);
 	if (Watson->owner == NULL)
 	{
-		free(Watson->owner);
+		free(Watson);
 		return (NULL);
 	}
 	Watson->owner = _strcpy(Watson->owner, owner);
