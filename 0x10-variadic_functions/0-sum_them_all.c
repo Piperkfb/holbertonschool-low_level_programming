79561f39d1674ee85637f7dc4d994a82a606199e
Piperkfb <piperkfb@gmail.com>
2023-03-05 08:46:18 -0800

    WOOHOO

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - takes all parameters and adds them
+ * @n: first number
+ * Return: The sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+variatya alla moteki
