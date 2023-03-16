56d66458903e4ea89eba00c144a14634fad2eb17
Piperkfb <piperkfb@gmail.com>
2023-03-16 14:07:05 -0700

    try this

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list - prints elemts of a list_t list
+ * @h: The list_t 
+ * 
+ * Return: The number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int cnt = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		cnt++;
+		h = h->next;
+	}
+}
