1d6c8c788e4d3e62d7bb651fbcd4d3d2be1cccbf
Piperkfb <piperkfb@gmail.com>
2023-03-19 16:06:59 -0700

    laaaast try

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int cnt;
 	listint_t *temp;
-	listint_t *dup;
+	listint_t *dup = *head;
 
 	if (dup == NULL)
 		return (-1);
