71c5b69df1f35ad5bcaa5087ba40c31000ee6e4c
Piperkfb <piperkfb@gmail.com>
2023-03-19 12:11:45 -0700

    closing

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at given position
+ * @head: The linked list
+ * @idx: The given position
+ * @n: the data to be added
+ * Return: Memory of new addition
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	
+}
