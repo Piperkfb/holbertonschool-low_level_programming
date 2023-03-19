2c1e13fbcc9ba719d3b0a6ea68ab0004a4c31a45
Piperkfb <piperkfb@gmail.com>
2023-03-19 11:34:34 -0700

    therewegp

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,16 +11,13 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int cnt = 0;
-	listint_t *temp;
 
 	if (head == NULL)
 		return (NULL);
 
 	for (cnt = 0; cnt < index; cnt++)
 	{
-		temp = head;
 		head = head->next;
-		free(temp);
 	}
 
 	if (head == NULL)
