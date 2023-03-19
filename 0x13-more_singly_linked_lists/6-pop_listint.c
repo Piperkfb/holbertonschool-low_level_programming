f1b6a61f2330eb5b9b68d73a9394f1144a2e29fc
Piperkfb <piperkfb@gmail.com>
2023-03-19 11:11:39 -0700

    trying

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,12 +9,16 @@
 int pop_listint(listint_t **head)
 {
 	int data;
+	listint_t *temp;
 
 	if (*head == NULL)
 		return (0);
 
+	temp = *head;
 	data = (*head)->n;
 	*head = (*head)->next;
+
+	free(temp);
 
 	return (data);
 
