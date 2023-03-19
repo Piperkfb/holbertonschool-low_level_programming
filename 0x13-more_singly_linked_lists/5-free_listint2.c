78c17ffdd002c10a1c2a7ea63f895235959886e6
Piperkfb <piperkfb@gmail.com>
2023-03-19 10:17:06 -0700

    testing

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a list thru pointer pointer
+ * @head: head of list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head->next;
+		free(*head);
+		*head = temp;
+	}
+
+	head = NULL;
+}
