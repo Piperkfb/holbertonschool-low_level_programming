7adb528cd91f28d781b809a6916441df37feceb1
Piperkfb <piperkfb@gmail.com>
2023-03-20 10:31:02 -0700

    first try

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - returns value of bit at given index
+ * @n: The number
+ * @index: the position
+ * Return: the value
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
+}
