13c6a510a3d6c9e443e0bf13649a804525744822
Piperkfb <piperkfb@gmail.com>
2023-03-22 11:29:11 -0700

    push

---
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a linked list
+ * @head: The linked list
+ * @index: The numerical position
+ * Return: Location of specified node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int cnt = 0;
+	dlistint_t *temp = head;
+
+	for (cnt= 0; cnt < index; cnt++)
+		temp = temp->next;
+
+
+}
