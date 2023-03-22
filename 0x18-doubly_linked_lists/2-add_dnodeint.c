e06971e7dff6371db9d3eaa33689bf6bc55f3b3d
Piperkfb <piperkfb@gmail.com>
2023-03-22 09:28:28 -0700

    eyyy there we go

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -9,23 +9,28 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *old;
+	dlistint_t *new;
+	dlistint_t *old;
 
-	new->next = NULL;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	
+	new->prev = NULL;
 	new->n = n;
 
 	if (*head == NULL)
 	{
-		new->prev = NULL;
-		head = new;
+		new->next = NULL;
+		*head = new;
 	}
 
 	else
 	{
-		old = head;
+		old = *head;
 		old->prev = new;
 		new->next = old;
-		head = new;
+		*head = new;
 	}
 	return (new);
 }
