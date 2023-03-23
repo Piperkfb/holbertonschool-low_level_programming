2727acdb7d7dac93ef712c9c0f7c0e950bc9b585
Piperkfb <piperkfb@gmail.com>
2023-03-23 14:05:41 -0700

    betty update on header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,17 +1,18 @@
 #ifndef HEADER_H
 #define HEADER_H
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
 /**
- *  * struct hash_node_s - Node of a hash table
- *   *
- *    * @key: The key, string
- *     * The key is unique in the HashTable
- *      * @value: The value corresponding to a key
- *       * @next: A pointer to the next node of the List
- *        */
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
 typedef struct hash_node_s
 {
 	char *key;
@@ -20,16 +21,16 @@ typedef struct hash_node_s
 } hash_node_t;
 
 /**
- *  * struct hash_table_s - Hash table data structure
- *   *
- *    * @size: The size of the array
- *     * @array: An array of size @size
- *      * Each cell of this array is a pointer to the first node of a linked list,
- *       * because we want our HashTable to use a Chaining collision handling
- *        */
+ * struct hash_table_s - Hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ */
 typedef struct hash_table_s
 {
-       	unsigned long int size;
+	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
 
