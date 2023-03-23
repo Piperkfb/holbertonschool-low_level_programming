46cf6fd54aee92093db9b3e497d05c972f55d9ce
Piperkfb <piperkfb@gmail.com>
2023-03-23 11:25:46 -0700

    first try

---
+++ 0x18-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Returns he sumod all the data in a linked list
+ * @head: The linked list
+ * Return: sum of all data
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head)
+	{
+		sum =+ head->n;
+		head = head->next'
+	}
+
+	return (sum);
+}
