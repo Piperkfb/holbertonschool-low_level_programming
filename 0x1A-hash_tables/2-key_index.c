5c0dcd1e33dbe8fff1bdfaf67b4521284fd15afa
Piperkfb <piperkfb@gmail.com>
2023-03-24 10:37:55 -0700

    weird

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives index of a key
+ * @key: The key
+ * @size:
+ * Return: The index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
