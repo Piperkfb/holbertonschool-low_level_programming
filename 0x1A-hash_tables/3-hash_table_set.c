2d57bd53a7c329ed2f432bd893d04068801fbf26
Piperkfb <piperkfb@gmail.com>
2023-03-24 12:32:40 -0700

    shut down

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an elements to a hash table
+ * @ht: Hash table
+ * @key: The element key
+ * @value: value along side key
+ * Return: 1 sucess, 0 other
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	
+}
