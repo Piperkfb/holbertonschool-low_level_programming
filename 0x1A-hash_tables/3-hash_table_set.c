e21a2307a6c5c687fc2bbf37a9a38667d975cb30
Piperkfb <piperkfb@gmail.com>
2023-03-25 09:58:16 -0700

    so sweet

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,5 +10,40 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	
+	unsigned long int idx;
+	unsigned long int i;
+	hash_node_t *new;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	idx = key_index((const unsigned char *) key, ht->size);
+	for (i = idx; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = strdup(value);
+			if (ht->array[i]->value == NULL)
+				return (0);
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+
+	new->key = strdup(key);
+	if (new->key == NULL)
+		return (0);
+
+	new->value = strdup(value);
+	if (new->value == NULL)
+		return (0);
+
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+
+	return (1);
 }
